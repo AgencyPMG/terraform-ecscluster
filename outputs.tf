86d153ccc7fe71700240625630963b13864a55a3
Eric Elsken <eric.elsken@gmail.com>
2018-07-26 17:32:21 -0500

    Add ebs_optimized variable to set on launch config

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,47 @@
 output "server_role_name" {
-    value = "${aws_iam_role.server.name}"
+  value = "${aws_iam_role.server.name}"
 }
 
 output "server_role_arn" {
-    value = "${aws_iam_role.server.name}"
+  value = "${aws_iam_role.server.name}"
 }
 
 output "server_instance_profile_id" {
-    value = "${aws_iam_instance_profile.server.id}"
+  value = "${aws_iam_instance_profile.server.id}"
 }
 
 output "ecs_role_name" {
-    value = "${aws_iam_role.ecs.name}"
+  value = "${aws_iam_role.ecs.name}"
 }
 
 output "ecs_role_arn" {
-    value = "${aws_iam_role.ecs.name}"
+  value = "${aws_iam_role.ecs.name}"
 }
 
 output "server_security_group_id" {
-    value = "${aws_security_group.server.id}"
+  value = "${aws_security_group.server.id}"
 }
 
 output "ecs_cluster_name" {
-    value = "${aws_ecs_cluster.main.name}"
+  value = "${aws_ecs_cluster.main.name}"
 }
 
 output "ecs_cluster_arn" {
-    value = "${aws_ecs_cluster.main.arn}"
+  value = "${aws_ecs_cluster.main.arn}"
 }
 
 output "launch_configuration_name" {
-    value = "${length(aws_launch_configuration.ecs.*.name) > 0 ? element(aws_launch_configuration.ecs.*.name, 0) : ""}"
+  value = "${length(aws_launch_configuration.ecs.*.name) > 0 ? element(aws_launch_configuration.ecs.*.name, 0) : ""}"
 }
 
 output "asg_id" {
-    value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.id, 0) : ""}"
+  value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.id, 0) : ""}"
 }
 
 output "asg_arn" {
-    value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.arn, 0) : ""}"
+  value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.arn, 0) : ""}"
 }
 
 output "asg_name" {
-    value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.name, 0) : ""}"
+  value = "${length(aws_autoscaling_group.ecs.*.id) > 0 ? element(aws_autoscaling_group.ecs.*.name, 0) : ""}"
 }
--- securitygroups.tf
+++ securitygroups.tf
@@ -1,9 +1,10 @@
 resource "aws_security_group" "server" {
-    name = "ecs-server@${var.cluster_name}"
-    description = "Default security group for the ECS servers"
-    vpc_id = "${var.vpc_id}"
-    tags {
-        Application = "${var.app}"
-        Environment = "${var.env}"
-    }
+  name        = "ecs-server@${var.cluster_name}"
+  description = "Default security group for the ECS servers"
+  vpc_id      = "${var.vpc_id}"
+
+  tags {
+    Application = "${var.app}"
+    Environment = "${var.env}"
+  }
 }
