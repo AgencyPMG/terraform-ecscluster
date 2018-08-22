fbcfb0aa7a5ce9ebaf15ff7341fb28662fdef907
Eric Elsken <eric.elsken@gmail.com>
2018-08-22 15:38:17 -0500

    Update security group description for multiple cluster distinguishes

--- securitygroups.tf
+++ securitygroups.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "server" {
   name        = "ecs-server@${var.cluster_name}"
-  description = "Default security group for the ECS servers"
+  description = "Default security group for the ${var.cluster_name} ECS servers"
   vpc_id      = "${var.vpc_id}"
 
   tags {
